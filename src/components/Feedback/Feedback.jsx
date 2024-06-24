c438571dc63158e28f884fb187773c885bc3452b
vladakolos <linikvladislava@gmail.com>
2024-06-24 22:29:57 +0800

    changes for all components

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,24 @@
-const Feedback = ({ text, value }) => {
+const Feedback = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <div>
-      <p>
-        {text}: {value}
-      </p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total feedbacks: {total}</p>
+      <p>Positive feedback percentage: {positivePercentage}%</p>
     </div>
   );
 };
 
 export default Feedback;
+// const Feedback = ({ text, value }) => {
+//   return (
+//     <div>
+//       <p>
+//         {text}: {value}
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default Feedback;
